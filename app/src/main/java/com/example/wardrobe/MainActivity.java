b5ce013f4e4d7fd922b654b2932178b009cac028
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-11-16 00:09:30 +0900

    read QRCode

--- app/src/main/java/com/example/wardrobe/MainActivity.java
+++ app/src/main/java/com/example/wardrobe/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.wardrobe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +26,18 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            Toast.makeText(this, scanResult.getContents(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override

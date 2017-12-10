4d1c92d1d330c55766a8ef5bb64a4f3a8e9f72cb
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-12-10 10:30:17 +0900

    start at QRCode-Reader

--- app/src/main/java/com/example/wardrobe/MainActivity.java
+++ app/src/main/java/com/example/wardrobe/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -19,13 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new IntentIntegrator(MainActivity.this).initiateScan(IntentIntegrator.QR_CODE_TYPES);
+
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new IntentIntegrator(MainActivity.this).initiateScan(IntentIntegrator.QR_CODE_TYPES);
-            }
-        });
     }
 
     @Override

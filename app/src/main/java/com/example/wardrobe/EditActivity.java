7f3653c66ce07da5fd601fb557a3217f38950390
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-12-21 17:18:05 +0900

    add EditActivity-layout

--- app/src/main/java/com/example/wardrobe/EditActivity.java
+++ app/src/main/java/com/example/wardrobe/EditActivity.java
@@ -3,16 +3,20 @@ package com.example.wardrobe;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class EditActivity extends CameraIntentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
 
         Uri imageUri = getIntent().getParcelableExtra(MainActivity.IMAGE_URI);
-        Toast.makeText(this, imageUri.toString(), Toast.LENGTH_SHORT).show();
+
+        Glide.with(this).load(imageUri).into((ImageView) findViewById(R.id.imageView));
     }
 
     @Override
